e31305e8efe297b1c70e8ad30eb2149f83cfb48f
sylvesterakrong <akrongsylvester@gmail.com>
2023-12-19 16:13:45 +0000

    Updated home screen and app icon

--- getitapp/lib/components/bottomnavbar.dart
+++ getitapp/lib/components/bottomnavbar.dart
@@ -1,52 +1,55 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//import 'package:cupertino_icons/cupertino_icons.dart';
 
-class ButtomNavBar extends StatelessWidget {
+class ButtomNavBar extends StatefulWidget {
   const ButtomNavBar(
       {super.key, required this.currentIndex, required this.onTap});
 
   final int currentIndex;
   final Function(int) onTap;
 
+  @override
+  State<ButtomNavBar> createState() => _ButtomNavBarState();
+}
+
+class _ButtomNavBarState extends State<ButtomNavBar> {
   BottomNavigationBarItem _buildTabItem(
       IconData icon, IconData filledIcon, int index, String label) {
     return BottomNavigationBarItem(
       icon: Icon(
-        currentIndex == index ? filledIcon : icon,
-        color: currentIndex == index ? Colors.red : Colors.grey,
+        widget.currentIndex == index ? filledIcon : icon,
+        color: widget.currentIndex == index ? Colors.red : Colors.grey,
       ),
       label: label,
+      backgroundColor: Colors.grey[850],
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    // ignore: avoid_unnecessary_containers
     return Container(
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(color: Colors.grey.withOpacity(0.7), blurRadius: 1.0)
-        ],
-      ),
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(3, 10, 3, 0),
+        padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
         child: ClipRRect(
-          borderRadius: BorderRadius.circular(12),
+          borderRadius: BorderRadius.circular(1),
           child: BottomNavigationBar(
-            currentIndex: currentIndex,
-            onTap: onTap,
-            showSelectedLabels: true, 
+            currentIndex: widget.currentIndex,
+            onTap: widget.onTap,
+            showSelectedLabels: true,
             showUnselectedLabels: true,
-            
+
             //height: 60,
-            backgroundColor: Colors.grey[200],
-            unselectedItemColor: Colors.white,
+            backgroundColor: Colors.grey,
+            //unselectedItemColor: Colors.white,
             selectedItemColor: Colors.red,
             items: [
               _buildTabItem(Icons.home, Icons.home, 0, 'Home'),
               _buildTabItem(Icons.book, Icons.book_outlined, 1, 'Novels'),
-              _buildTabItem(Icons.my_library_books_sharp, Icons.library_books_outlined, 2, 'My Library'),
-              _buildTabItem(CupertinoIcons.bag, CupertinoIcons.bag_fill, 3, 'My Store'),
+              _buildTabItem(Icons.my_library_books_sharp,
+                  Icons.library_books_outlined, 2, 'My Library'),
+              _buildTabItem(
+                  CupertinoIcons.bag, CupertinoIcons.bag_fill, 3, 'My Store'),
             ],
           ),
         ),
--- getitapp/lib/pages/home_page.dart
+++ getitapp/lib/pages/home_page.dart
@@ -12,29 +12,66 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("H O M E"),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight + 20),
+        child: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Row(
+            children: [
+              IconButton(
+                color: Colors.grey,
+                onPressed: () {},
+                icon: const Icon(Icons.dehaze),
+                iconSize: 30,
+              ),
+              Center(
+                child: Container(
+                  height: 40,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(110, 245, 245, 245),
+                    borderRadius: BorderRadius.circular(5),
+                    //contentPadding: EdgeInsets.all(20),
+                  ),
+                  child: TextField(
+                    //controller: _searchController,
+                    onChanged: (context) {},
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(10, 1, 10, 0),
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: InputBorder.none,
+                      hintText: ' Search...',
+                      suffixIcon: Icon(
+                        Icons.mic,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
+
+      backgroundColor: Colors.black,
+
       body: ListView(
-        children: [
-          Container(
-            height: 350,
-            color: Colors.grey,
+
+          // children: [
+          //   Container(
+          //     height: 350,
+          //     color: Colors.grey,
+          //   ),
+          //   const SizedBox(
+          //     height: 5,
+          //   ),
+          //   Container(
+          //     height: 350,
+          //     color: Colors.grey[200],
+          //   ),
+          // ],
           ),
-          const SizedBox(
-            height: 5,
-          ),
-          Container(
-            height: 350,
-            color: Colors.grey[200],
-          ),
-        ],
-      ),
-
-
-
-
-
 
       //bottom navigator
       bottomNavigationBar: ButtomNavBar(
